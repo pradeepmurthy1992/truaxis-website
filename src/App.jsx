eb0e7edee628fec4792b2bfd83b9faad31d08821
pradeepmurthy1992 <pradeepmurthy8@gmail.com>
2026-07-01 20:17:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import Navbar from "./components/layout/Navbar";
 import Hero from "./features/hero/Hero";
 
 function App() {
-  return <Hero />;
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
 }
 
 export default App;

d29d23ea5d387ae364c56a7322d69a4fd2ac2cbd
pradeepmurthy1992 <pradeepmurthy8@gmail.com>
2026-07-01 20:25:33 +0530

    Create Process.jsx

---
+++ src/features/process/Process.jsx
@@ -0,0 +1,19 @@
+function Process() {
+  return (
+    <section id="process">
+      <p>OUR PROCESS</p>
+
+      <h2>Simple & Transparent</h2>
+
+      <ol>
+        <li>Receive Enquiry</li>
+        <li>Schedule Inspection</li>
+        <li>Conduct Site Inspection</li>
+        <li>Prepare Inspection Report</li>
+        <li>Submit Final Report</li>
+      </ol>
+    </section>
+  );
+}
+
+export default Process;

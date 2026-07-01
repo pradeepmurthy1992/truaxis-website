686c4d5ee892554ceac2347d281372fe00657858
pradeepmurthy1992 <pradeepmurthy8@gmail.com>
2026-07-01 20:25:06 +0530

    Create Industries.jsx

---
+++ src/features/industries/Industries.jsx
@@ -0,0 +1,28 @@
+function Industries() {
+  const industries = [
+    "Commercial Buildings",
+    "Residential Apartments",
+    "Shopping Malls",
+    "Hospitals",
+    "Hotels",
+    "Airports",
+    "Educational Institutions",
+    "Industrial Facilities",
+  ];
+
+  return (
+    <section id="industries">
+      <p>INDUSTRIES WE SERVE</p>
+
+      <h2>Trusted Across Multiple Sectors</h2>
+
+      <ul>
+        {industries.map((industry) => (
+          <li key={industry}>{industry}</li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+export default Industries;

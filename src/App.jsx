22f68b131899f461c13cd24892178c0dc5c51d87
pradeepmurthy1992 <pradeepmurthy8@gmail.com>
2026-07-01 20:20:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/layout/Navbar";
 import Hero from "./features/hero/Hero";
 import About from "./features/about/About";
 import Services from "./features/services/Services";
+import Enquiry from "./features/enquiry/Enquiry";
 
 function App() {
   return (

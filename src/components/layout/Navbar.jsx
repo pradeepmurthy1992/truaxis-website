6a161e511a71aabd200da3aa5fb21756e86a196f
pradeepmurthy1992 <pradeepmurthy8@gmail.com>
2026-07-01 21:02:15 +0530

    Update Navbar.jsx

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -3,18 +3,27 @@ import Container from "./Container";
 
 function Navbar() {
   return (
-    <header>
+    <header className="navbar-wrapper">
       <Container>
         <div className="navbar">
-          <h2>TRUAXIS</h2>
 
-          <nav>
+          <div className="logo">
+            <h2>TRUAXIS</h2>
+            <small>Inspection Services</small>
+          </div>
+
+          <nav className="nav-links">
             <a href="#home">Home</a>
             <a href="#about">About</a>
             <a href="#services">Services</a>
             <a href="#enquiry">Enquiry</a>
             <a href="#contact">Contact</a>
           </nav>
+
+          <a href="#enquiry" className="nav-button">
+            Book Inspection
+          </a>
+
         </div>
       </Container>
     </header>

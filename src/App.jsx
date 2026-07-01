c249af669da3d42407121305ed128d62198650c5
pradeepmurthy1992 <pradeepmurthy8@gmail.com>
2026-07-01 20:21:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./features/hero/Hero";
 import About from "./features/about/About";
 import Services from "./features/services/Services";
 import Enquiry from "./features/enquiry/Enquiry";
+import Contact from "./features/contact/Contact";
 
 function App() {
   return (

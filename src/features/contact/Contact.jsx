06fc2399c12aed8a1ad58f184a9f35c4b02d7d09
pradeepmurthy1992 <pradeepmurthy8@gmail.com>
2026-07-01 20:21:14 +0530

    Create Contact.jsx

---
+++ src/features/contact/Contact.jsx
@@ -0,0 +1,33 @@
+function Contact() {
+  return (
+    <section id="contact">
+      <p>CONTACT US</p>
+
+      <h2>Let's Talk About Your Inspection Requirements</h2>
+
+      <div>
+        <div>
+          <h3>Office</h3>
+
+          <p>TRUAXIS</p>
+
+          <p>Bengaluru, Karnataka, India</p>
+
+          <p>GSTIN: XXXXXXXXXXXXXXX</p>
+        </div>
+
+        <div>
+          <h3>Get in Touch</h3>
+
+          <p>Email: info@truaxis.in</p>
+
+          <p>Phone: +91 XXXXXXXXXX</p>
+
+          <p>Working Hours: Mon - Sat | 9:00 AM - 6:00 PM</p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Contact;

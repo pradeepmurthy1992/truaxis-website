fbca5c7404e978f470d5b2502dd02d3ff4a4e56b
pradeepmurthy1992 <pradeepmurthy8@gmail.com>
2026-07-01 21:01:43 +0530

    Update Navbar.jsx

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,19 +1,22 @@
 import React from "react";
+import Container from "./Container";
 
 function Navbar() {
   return (
     <header>
-      <div>
-        <h2>TRUAXIS</h2>
+      <Container>
+        <div className="navbar">
+          <h2>TRUAXIS</h2>
 
-        <nav>
-          <a href="#home">Home</a>
-          <a href="#about">About</a>
-          <a href="#services">Services</a>
-          <a href="#enquiry">Enquiry</a>
-          <a href="#contact">Contact</a>
-        </nav>
-      </div>
+          <nav>
+            <a href="#home">Home</a>
+            <a href="#about">About</a>
+            <a href="#services">Services</a>
+            <a href="#enquiry">Enquiry</a>
+            <a href="#contact">Contact</a>
+          </nav>
+        </div>
+      </Container>
     </header>
   );
 }

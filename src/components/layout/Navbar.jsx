b3f3f49823d235d9931cea5e066507e1c486acda
pradeepmurthy1992 <pradeepmurthy8@gmail.com>
2026-07-01 20:17:41 +0530

    Update Navbar.jsx

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,1 +1,20 @@
 
+function Navbar() {
+  return (
+    <header>
+      <div>
+        <h2>TRUAXIS</h2>
+
+        <nav>
+          <a href="#home">Home</a>
+          <a href="#about">About</a>
+          <a href="#services">Services</a>
+          <a href="#enquiry">Enquiry</a>
+          <a href="#contact">Contact</a>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;

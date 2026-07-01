602d18b105153589a4d4df25663878d717da3417
pradeepmurthy1992 <pradeepmurthy8@gmail.com>
2026-07-01 21:39:04 +0530

    Update Navbar.jsx

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -4,26 +4,46 @@ import Container from "./Container";
 function Navbar() {
   return (
     <header className="navbar-wrapper">
+
       <Container>
+
         <div className="navbar">
+
           <a href="#home" className="logo">
+
             <h2>TRUAXIS</h2>
-            <span>Independent Inspection Services</span>
+
+            <span>
+              Independent Lift & Escalator Inspection Services
+            </span>
+
           </a>
 
           <nav className="nav-links">
+
             <a href="#about">About</a>
+
             <a href="#services">Services</a>
+
             <a href="#industries">Industries</a>
+
             <a href="#process">Process</a>
+
             <a href="#contact">Contact</a>
+
           </nav>
 
-          <a href="#enquiry" className="nav-button">
+          <a
+            href="#enquiry"
+            className="nav-button"
+          >
             Request Inspection
           </a>
+
         </div>
+
       </Container>
+
     </header>
   );
 }

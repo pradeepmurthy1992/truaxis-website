e1692fc3b8928c15d8e569b8f1e3b75229f1b917
pradeepmurthy1992 <pradeepmurthy8@gmail.com>
2026-07-01 19:38:38 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+function App() {
+  return (
+    <div>
+      {/* Navbar */}
+      <header>
+        <h2>TRUAXIS</h2>
+
+        <nav>
+          <a href="#home">Home</a>
+          <a href="#about">About</a>
+          <a href="#services">Services</a>
+          <a href="#contact">Contact</a>
+        </nav>
+      </header>
+
+      {/* Hero */}
+      <main id="home">
+        <h1>Independent Lift & Escalator Inspection Services</h1>
+
+        <p>
+          Reliable • Independent • Professional
+        </p>
+
+        <button>Book Inspection</button>
+        <button>Enquiry</button>
+      </main>
+    </div>
+  );
+}
+
+export default App;

e1dd533964eac39d819f89fb772517fdd420f089
pradeepmurthy1992 <pradeepmurthy8@gmail.com>
2026-07-01 20:18:35 +0530

    Create About.jsx

---
+++ src/features/about/About.jsx
@@ -0,0 +1,20 @@
+function About() {
+  return (
+    <section id="about">
+      <div>
+        <p>ABOUT TRUAXIS</p>
+
+        <h2>Independent Inspection. Trusted Expertise.</h2>
+
+        <p>
+          TRUAXIS provides independent lift and escalator inspection services
+          to help clients ensure safety, compliance, and operational
+          reliability. We focus solely on inspection services with an
+          impartial and professional approach.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
